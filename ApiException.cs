ed5ce00da7ee97ed2174a9fbece9f422f3f5efb0
Junaid <junaid2396@hotmail.com>
2024-06-03 01:35:59 +0100

    Improved Error handling

---
+++ ApiException.cs
@@ -0,0 +1,9 @@
+﻿public class ApiException : Exception
+{
+    public int StatusCode { get; }
+
+    public ApiException(int statusCode, string message) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
